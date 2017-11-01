753a2d95921fb5d7038bc1597de7522449e75adf
Matt Bernier <matt.bernier@sendgrid.com>
2017-10-31 21:57:21 -0600

    Changed license to look for .txt or .md :)

--- test/test_ruby_http_client.rb
+++ test/test_ruby_http_client.rb
@@ -213,7 +213,7 @@ class TestClient < Minitest::Test
   end
 
   def test_license_exists
-    assert(File.file?('./LICENSE.md'))
+    assert(File.file?('./LICENSE.md') || File.file?('./LICENSE.txt'))
   end
 
   def test_pull_request_template_exists
